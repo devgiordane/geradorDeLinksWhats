ec93fc2c0b0ea418e6a0eb9b3352ee3e50c5133a
devgiordane <bruno_giordane@outlook.com>
2020-07-25 00:00:17 -0300

    Versão 1.0

---
+++ src/components/Gerador.vue
@@ -0,0 +1,44 @@
+<template>
+  <div class="col-12 py-5">
+    <div class="form-group">
+      <label for="">NÚMERO</label>
+      <input type="text" v-model="numero" class="form-control" />
+    </div>
+    <div class="form-group">
+      <label for="">MENSAGEM</label>
+      <input type="text" v-model="mensagem" class="form-control" />
+    </div>
+    <div class="">
+      <button @click="Geralink()" class="btn btn-primary btn-lg btn-block">
+        GERAR LINK
+      </button>
+    </div>
+    {{ url }}
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Gerador",
+  data: function() {
+    return {
+      numero: "",
+      mensagem: "",
+      url: "",
+    };
+  },
+  methods: {
+    Geralink: function() {
+      let n = this.numero.replace(/[^0-9]/g, "");
+      let m = encodeURI(this.mensagem);
+      this.url = `https://wa.me/send?phone=${n}&text=${m}`;
+      this.$router.push({
+        name: "Resultado",
+        params: {
+          link: this.url,
+        },
+      });
+    },
+  },
+};
+</script>
---
+++ src/views/Resultado.vue
@@ -0,0 +1,36 @@
+<template>
+  <div class="container">
+    <div class="row">
+      <div class="col-12">
+        <div class="form-group">
+          <label for="exampleFormControlTextarea1">SEU LINK FOI GERADO</label>
+          <textarea
+            class="form-control"
+            id="url"
+            rows="3"
+            v-model="$route.params.link"
+          ></textarea>
+        </div>
+      </div>
+      <div class="col-12 d-flex justify-content-around">
+        <button
+          id="copiar"
+          data-clipboard-target="#url"
+          class="btn btn-success"
+        >
+          COPIAR
+        </button>
+        <a target="_blank" :href="$route.params.link" class="btn btn-warning"
+          >TESTAR</a
+        >
+        <router-link to="/" class="btn btn-danger">VOLTAR</router-link>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import ClipboardJS from "clipboard";
+new ClipboardJS("#copiar");
+export default {};
+</script>
